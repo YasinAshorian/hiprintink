b57d84c69439214f9afc4817d5c98107ce3a8e3b
yasinashourian <yasinashourian@gmail.com>
2025-12-11 15:36:02 +0330

    develop

--- resources/views/livewire/components/nav/navbar.blade.php
+++ resources/views/livewire/components/nav/navbar.blade.php
@@ -1,33 +1,35 @@
-<div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+<div class="container-fluid position-relative d-flex align-items-center justify-content-between">
 
     <a href="#" class="logo d-flex align-items-center">
         <img src="{{ asset('assets/img/hi-logo.jpg') }}" alt="hi-logo">
     </a>
 
-    <nav id="navmenu" class="navmenu" dir="rtl">
-        <ul>
-            <li><a href="{{ route('home-page') }}" class="{{ url()->full() == route('home-page') ? "active" : "" }}">صفحه
-                    اصلی </a></li>
+    <div class="container">
+        <nav id="navmenu" class="navmenu" dir="rtl">
+            <ul>
+                <li><a href="{{ route('home-page') }}" class="{{ url()->full() == route('home-page') ? "active" : "" }}">صفحه
+                        اصلی </a></li>
 
-            <li><a href="{{ route('about-us') }}" class="{{ url()->full() == route('about-us') ? "active" : "" }}">درباره
-                    ما</a></li>
+                <li><a href="{{ route('about-us') }}" class="{{ url()->full() == route('about-us') ? "active" : "" }}">درباره
+                        ما</a></li>
 
-            @foreach($productGroups as $item)
+                @foreach($productGroups as $item)
 
-                <li class="dropdown"><a href="#"><span>{{ $item->name }} </span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                    <ul>
-                        @foreach($item->productItems as $i)
-                            <li><a href="{{ route('products', ['group_item' => $i]) }}">{{ $i->name }}</a></li>
-                        @endforeach
-                    </ul>
-                </li>
+                    <li class="dropdown"><a href="#"><span>{{ $item->name }} </span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                        <ul>
+                            @foreach($item->productItems as $i)
+                                <li><a href="{{ route('products', ['group_item' => $i]) }}">{{ $i->name }}</a></li>
+                            @endforeach
+                        </ul>
+                    </li>
 
-            @endforeach
+                @endforeach
 
-            <li><a href="{{ route('blogs') }}" class="{{ url()->full() == route('blogs') ? "active" : "" }}"> وبلاگ  </a></li>
-            <li><a href="{{ route('contact-us') }}" class="{{ url()->full() == route('contact-us') ? "active" : "" }}">ارتباط با ما</a></li>
-        </ul>
+                <li><a href="{{ route('blogs') }}" class="{{ url()->full() == route('blogs') ? "active" : "" }}"> وبلاگ  </a></li>
+                <li><a href="{{ route('contact-us') }}" class="{{ url()->full() == route('contact-us') ? "active" : "" }}">ارتباط با ما</a></li>
+            </ul>
 
-        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-    </nav>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+    </div>
 </div>
